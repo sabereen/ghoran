8430611ee7606bae4226c12c9fac109fa4235ef0
sabereen <mqtstbh@gmail.com>
2024-04-01 18:12:18 +0330

    feat: add quran text

---
+++ packages/text/scripts/extract-hafs-v22.js
@@ -0,0 +1,32 @@
+import { extractRawText } from 'mammoth'
+import { writeFileSync, readFileSync } from 'node:fs'
+import assert from 'assert'
+
+const suraNameRegex = /سُورَةُ (.*?)\n\n/gm
+const removableBasmalahRegex = /بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ\n\n/gm
+
+export async function extractHafsV22() {
+  const content = readFileSync(new URL('../raw-text/uthmanic-hafs-v22.docx', import.meta.url))
+
+  const result = await extractRawText({
+    buffer: content,
+  })
+
+  let text = result.value
+
+  assert.equal([...text.matchAll(suraNameRegex)].length, 114)
+  // assert.equal([...text.matchAll(removableBasmalahRegex)].length, 114 - 2)
+
+  text = text
+    .replace(removableBasmalahRegex, '')
+    .replace(suraNameRegex, '')
+
+  const ayat = text.trim()
+    .split(/[٠-٩]+/g)
+    .map(text => text.trim())
+    .filter(Boolean)
+
+  assert.equal(ayat.length, 6236)
+
+  writeFileSync('./text/quran-text-hafs-v22.json', JSON.stringify(ayat, null, 2))
+}
---
+++ packages/text/scripts/extract-text.js
@@ -0,0 +1,6 @@
+
+import { extractHafsV13 } from './extract-hafs-v13.js'
+import { extractHafsV22 } from './extract-hafs-v22.js'
+
+extractHafsV13()
+extractHafsV22()
