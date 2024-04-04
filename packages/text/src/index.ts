168de0c20e2d06df20816dca0d5e52b30553ba38
sabereen <mqtstbh@gmail.com>
2024-04-04 05:03:18 +0330

    feat: handle caching quran text

--- packages/text/src/index.ts
+++ packages/text/src/index.ts
@@ -1,6 +1,22 @@
 import { QuranTextType } from './types'
 
-function importText(type: QuranTextType): Promise<{ default: string[] }> {
+/**
+ * به علت اینکه از نظر تایپ اسکریپتی ممکن است این فایل
+ * درون محیط نودجی‌اس اجرا شود به کش‌استوریج دسترسی نداریم.
+ * ما برای اینکه به مشکلی نخوریم آن را تعریف می‌کنیم ولی درون
+ * try catch
+ * از آن استفاده می‌کنیم که در محیط نود هم مشکلی پیش نیاید.
+ */
+declare const caches: any
+
+/**
+ * مستقیما جیسون مربوط به متن قرآن را ایمپورت می‌کند
+ * @param type - نوع متن قرآن
+ * @returns خروجی مستقیم ایمپورت پویا
+ */
+export function importText(
+  type: QuranTextType,
+): Promise<{ default: string[] }> {
   switch (type) {
     case 'clean':
       return import('../json/quran-text-clean.json')
@@ -15,6 +31,35 @@ function importText(type: QuranTextType): Promise<{ default: string[] }> {
   }
 }
 
-export function loadText(type: QuranTextType) {
-  return importText(type).then((module) => module.default)
+/**
+ * بر اساس نوع ورودی متن قرآن متناظر را بارگیری می‌کند
+ * و نتیجه‌ی آن را کش می‌کند
+ * @param type - نوع متن قرآن
+ */
+export async function loadText(
+  type: QuranTextType,
+  { cacheName = '@ghoran', cachePrefix = '@ghoran' } = {},
+) {
+  const cacheKey = `/${cachePrefix}/quran-text/${type}`
+  try {
+    const cache = await caches.open(cacheName)
+    const response = await cache.match(cacheKey)
+    if (response) {
+      const text = await response.json()
+      return text
+    }
+  } catch (err) {}
+  const result = await importText(type)
+  const text = result.default
+  try {
+    const cache = await caches.open(cacheName)
+    await cache.put(
+      cacheKey,
+      new Response(JSON.stringify(text), {
+        headers: { 'content-type': 'application/json' },
+      }),
+    )
+  } finally {
+    return text
+  }
 }
