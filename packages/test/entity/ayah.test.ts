f816de5fce4397ed4ac2d76a678717dae238473e
sabereen <mqtstbh@gmail.com>
2024-04-05 20:12:43 +0330

    chore: add test for ayah

--- packages/test/entity/ayah.test.ts
+++ packages/test/entity/ayah.test.ts
@@ -5,4 +5,14 @@ describe('Ayah class', () => {
     expect(() => Ayah.get(-1)).toThrow()
     expect(() => Ayah.get(6236)).toThrow()
   })
+
+  test('first ayah of baqarah', () => {
+    const ayah = Ayah.get(7)
+    expect(ayah.surahNumber).toBe(2)
+    expect(ayah.ayahNumber).toBe(1)
+    expect(ayah.pageNumber).toBe(2)
+    expect(ayah.juzNumber).toBe(1)
+    expect(ayah.sajdah).toBe(false)
+    expect(ayah.obligatorySajdah).toBe(false)
+  })
 })
