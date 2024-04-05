577fb7209d8c2dee31e7703f9d3e29ea4a498a0d
sabereen <mqtstbh@gmail.com>
2024-04-05 06:34:50 +0330

    chore: add Ayah class

--- packages/entity/src/ayah.ts
+++ packages/entity/src/ayah.ts
@@ -1,1 +1,25 @@
-export class Ayah {}
+import { SurahTuple, surahList } from '@ghoran/metadata'
+import { searchBinaryForAyah } from '@ghoran/utils'
+
+function getIndexOfFirstAyah(surah: SurahTuple) {
+  return surah[0]
+}
+
+export class Ayah {
+  private constructor(readonly index: number) {}
+
+  static get(index: number) {
+    return new Ayah(index)
+  }
+
+  static fromSurahNumber(surahNumber: number, ayahNumberInSurah: number) {
+    const surah = surahList[surahNumber - 1]
+    if (!surah) return null
+    const index = surah[0] + ayahNumberInSurah
+    return Ayah.get(index)
+  }
+
+  surahIndex() {
+    return searchBinaryForAyah(surahList, this.index, getIndexOfFirstAyah)
+  }
+}
