de6b6f5c84eeaa1ac725b6026fd59203385d28e3
sabereen <mqtstbh@gmail.com>
2024-04-05 05:04:45 +0330

    feat: add entity package

--- alias.ts
+++ alias.ts
@@ -8,4 +8,5 @@ export const alias: Record<string, string> = {
   '@ghoran/text/fonts': r('./packages/text/dist/fonts'),
   '@ghoran/text': r('./packages/text/src/'),
   '@ghoran/metadata': r('./packages/metadata/src/'),
+  '@ghoran/entity': r('./packages/entity/src/'),
 }
