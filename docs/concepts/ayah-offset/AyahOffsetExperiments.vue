ed273574837728e4ddd26d8a759d7b93f261d9bb
sabereen <mqtstbh@gmail.com>
2024-04-10 06:29:02 +0330

    chore: handle autofix

--- docs/concepts/ayah-offset/AyahOffsetExperiments.vue
+++ docs/concepts/ayah-offset/AyahOffsetExperiments.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { loadText } from '@ghoran/text'
-import { computed, ref } from 'vue'
+import { computed, ref, watch, watchSyncEffect } from 'vue'
 
 const mode = ref<'word' | 'alphabet'>('word')
+const autofix = ref(false)
 
 const [hafsText, imlaText] = await Promise.all([
   loadText('hafs'),
@@ -18,47 +19,108 @@ const wordStrategy = {
       alphabetStrategy.splitImla(imlaItem).length
     )
   },
-  test() {
-    const hafsWordCounts = hafsText.map(wordStrategy.splitHafs)
-    const imlaWordCounts = imlaText.map(wordStrategy.splitImla)
+  test(textsToUse: { hafs: string[]; imla: string[] }) {
+    const hafsWordCounts = textsToUse.hafs.map(wordStrategy.splitHafs)
+    const imlaWordCounts = textsToUse.imla.map(wordStrategy.splitImla)
     const errors = []
     for (let i = 0; i < hafsWordCounts.length; i++) {
       if (hafsWordCounts[i].length !== imlaWordCounts[i].length) errors.push(i)
     }
     return errors
   },
+  fixOneItem(index: number) {
+    const hafs = wordStrategy.splitHafs(texts.value.hafs[index])
+    const imla = wordStrategy.splitImla(texts.value.imla[index])
+    const length = Math.max(hafs.length, imla.length)
+    for (let i = 0; i < length; i++) {
+      if (!hafs[i] || !imla[i]) continue
+      if (!wordStrategy.match(hafs[i], imla[i])) {
+        const hafsLength = alphabetStrategy.splitHafs(hafs[i]).length
+        const imlaLength = alphabetStrategy.splitImla(imla[i]).length
+        if (imlaLength < hafsLength) {
+          // فاصله عادی را تبدیل به nbsp می‌کنیم
+          imla[i] = imla[i] + '\u00A0' + imla[i + 1]
+          imla.splice(i + 1, 1)
+        }
+      }
+    }
+
+    return { hafs, imla }
+  },
+  fix() {
+    const fixedImlaText = [...imlaText]
+    const fixedHafsText = [...hafsText]
+    const errors = wordStrategy.test({ hafs: hafsText, imla: imlaText })
+
+    errors.forEach((index) => {
+      const fixResult = wordStrategy.fixOneItem(index)
+      fixedHafsText[index] = fixResult.hafs.join(' ')
+      fixedImlaText[index] = fixResult.imla.join(' ')
+    })
+
+    console.log('fix word', { fixedImlaText, fixedHafsText })
+
+    return {
+      imla: fixedImlaText,
+      hafs: fixedHafsText,
+    }
+  },
+  getText() {
+    console.log('text word')
+    if (autofix.value) {
+      return wordStrategy.fix()
+    }
+    return {
+      hafs: hafsText,
+      imla: imlaText,
+    }
+  },
 }
 
 const alphabetStrategy = {
-  // اگر یک فتحه و الف مقصوره پشت سر هم دیده شد یک حرف به حساب بیاید
   splitHafs: (ayah: string): string[] =>
-    Array.from(ayah.match(/(\p{L}|\u064e\u0670)/gu) || []),
-  // الف مقصوره در رسم املاء پایه‌هایی همچون «ی» و «و» و «ا» دارد
-  // ضمن اینکه برای لفظ جلاله «الله» هم تشدبد قبل الف مقصوره است
-  // که در اینجا نباید آن را یک حرف مستقل به حساب آوریم
+    Array.from((ayah || '').match(/\p{L}/gu) || []).filter(
+      (item) => !item.match(/[وـؤءإٱأائىیيۦ]/),
+    ),
   splitImla: (ayah: string): string[] =>
-    ayah.match(/(\p{L}|(?<![اّو])\u0670(?=ی))/gu) || [],
+    Array.from((ayah || '').match(/\p{L}/gu) || []).filter(
+      (item) => !item.match(/[وـؤءإٱأائىیيۦ]/),
+    ),
   match(hafsItem: string, imlaItem: string) {
     return hafsItem === imlaItem
   },
-  test() {
-    const hafsWordCounts = hafsText.map(alphabetStrategy.splitHafs)
-    const imlaWordCounts = imlaText.map(alphabetStrategy.splitImla)
+  test(textsToUse: { hafs: string[]; imla: string[] }) {
+    const hafsWordCounts = textsToUse.hafs.map(alphabetStrategy.splitHafs)
+    const imlaWordCounts = textsToUse.imla.map(alphabetStrategy.splitImla)
     const errors = []
     for (let i = 0; i < hafsWordCounts.length; i++) {
       if (hafsWordCounts[i].length !== imlaWordCounts[i].length) {
-        console.log('error', hafsWordCounts[i], imlaWordCounts[i])
         errors.push(i)
       }
     }
     return errors
+  },
+  fix() {
+    return alphabetStrategy.getText()
+  },
+  getText() {
+    return {
+      hafs: hafsText,
+      imla: imlaText,
+    }
   },
 }
 
 const getStrategy = () =>
   mode.value === 'word' ? wordStrategy : alphabetStrategy
 
-const affectiveCountTestResult = computed(() => getStrategy().test())
+const texts = ref(getStrategy().getText())
+
+watch(autofix, () => {
+  texts.value = getStrategy().getText()
+})
+
+const affectiveCountTestResult = computed(() => getStrategy().test(texts.value))
 
 const expandCompare = ref({
   hafs: [] as string[],
@@ -68,10 +130,11 @@ const expandCompare = ref({
 
 function expand(i: number) {
   expandCompare.value = {
-    hafs: getStrategy().splitHafs(hafsText[i]),
-    imla: getStrategy().splitImla(imlaText[i]),
+    hafs: getStrategy().splitHafs(texts.value.hafs[i]),
+    imla: getStrategy().splitImla(texts.value.imla[i]),
     index: i,
   }
+  previewAutofix(i)
 }
 
 function isItemError(i: number) {
@@ -79,6 +142,27 @@ function isItemError(i: number) {
     expandCompare.value.hafs[i] || '',
     expandCompare.value.imla[i] || '',
   )
+}
+
+const autofixPreviewCompare = ref({
+  hafs: [] as string[],
+  imla: [] as string[],
+  index: -1,
+})
+function previewAutofix(index: number) {
+  const strategy = getStrategy()
+  if (strategy === alphabetStrategy) {
+    autofixPreviewCompare.value = {
+      hafs: [] as string[],
+      imla: [] as string[],
+      index: -1,
+    }
+    return
+  }
+
+  const { hafs, imla } = wordStrategy.fixOneItem(index)
+
+  autofixPreviewCompare.value = { hafs, imla, index }
 }
 </script>
 
@@ -94,6 +178,12 @@ function isItemError(i: number) {
         تفکیک حروف الفبا
       </label>
     </div>
+    <div>
+      <label>
+        <input type="checkbox" v-model="autofix" />
+        تلاش برای رفع اشکالات
+      </label>
+    </div>
     <div>تعداد آیات متفاوت: {{ affectiveCountTestResult.length }}</div>
     <div>
       <table>
@@ -104,8 +194,8 @@ function isItemError(i: number) {
         <tbody>
           <template v-for="index in affectiveCountTestResult">
             <tr @click="expand(index)">
-              <td>{{ hafsText[index] }}</td>
-              <td>{{ imlaText[index] }}</td>
+              <td>{{ texts.hafs[index] }}</td>
+              <td>{{ texts.imla[index] }}</td>
             </tr>
             <tr v-if="expandCompare.index === index">
               <td colspan="2" class="h-40 relative">
@@ -122,6 +212,28 @@ function isItemError(i: number) {
                     <td
                       v-for="(item, i) in expandCompare.imla"
                       :class="{ 'bg-red-500/15!': isItemError(i) }"
+                    >
+                      {{ item }}
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+            <tr v-if="autofixPreviewCompare.index === index">
+              <td colspan="2" class="h-40 relative">
+                <table class="absolute inset-0">
+                  <tr>
+                    <td
+                      v-for="(item, i) in autofixPreviewCompare.hafs"
+                      :class="{ 'bg-red-500/15!': false }"
+                    >
+                      {{ item }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td
+                      v-for="(item, i) in autofixPreviewCompare.imla"
+                      :class="{ 'bg-red-500/15!': false }"
                     >
                       {{ item }}
                     </td>
