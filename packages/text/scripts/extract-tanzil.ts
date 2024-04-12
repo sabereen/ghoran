897712c10a4838c65750ff972836c711e2f5eeb4
sabereen <mqtstbh@gmail.com>
2024-04-12 10:13:00 +0330

    chore: prefix tanzil

--- packages/text/scripts/extract-tanzil.ts
+++ packages/text/scripts/extract-tanzil.ts
@@ -17,6 +17,6 @@ export function extractTanzilText(name: string) {
   const path = r(`../raw-text/tanzil/${name}.txt`)
   const text = fs.readFileSync(path).toString()
   const textAsArray = text.split('\n').slice(0, 6236)
-  const finalPath = r(`../json/${name}-tanzil.json`)
+  const finalPath = r(`../json/tanzil-${name}.json`)
   fs.writeFileSync(finalPath, JSON.stringify(textAsArray, null, 2))
 }
