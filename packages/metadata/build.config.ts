30a74189dc8ab876e435d071ea312c46fda8b82c
sabereen <mqtstbh@gmail.com>
2024-04-12 12:33:50 +0330

    chore: add constants file

--- packages/metadata/build.config.ts
+++ packages/metadata/build.config.ts
@@ -1,7 +1,7 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: ['./src/index'],
+  entries: ['./src/index', './src/constants'],
 
   // Generates .d.ts declaration file
   declaration: true,
---
+++ packages/metadata/src/constants.ts
@@ -0,0 +1,5 @@
+/** count of all quran ayahs */
+export const COUNT_OF_AYAHS = 6236
+
+/** count of all surahs of quran */
+export const COUNT_OF_SURAHS = 114
