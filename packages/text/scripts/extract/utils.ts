44cd0a1db5272862405c6d84a00e87f71d10ddb0
sabereen <mqtstbh@gmail.com>
2024-04-16 21:43:11 +0330

    chore: add comment

--- packages/text/scripts/extract/utils.ts
+++ packages/text/scripts/extract/utils.ts
@@ -3,4 +3,5 @@ import { resolve } from 'node:path'
 
 const dirname = fileURLToPath(new URL('./', import.meta.url))
 
+// مسیری را می‌گیرد و یک مسیر مطلق از پوشه روت همین پکیج (پکیج متن) بر می‌گرداند
 export const resolvePath = (path: string) => resolve(dirname, '../..', path)
