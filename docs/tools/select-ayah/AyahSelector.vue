7fd922fc8c5d18414bb2f706a4049637f11de9b4
sabereen <mqtstbh@gmail.com>
2024-04-19 10:47:54 +0330

    chore: better ui

--- docs/tools/select-ayah/AyahSelector.vue
+++ docs/tools/select-ayah/AyahSelector.vue
@@ -40,29 +40,32 @@ const pageAyat = computed(() =>
       />
     </div>
     <div v-if="isValidPageNumber">
-      <div v-for="(ayah, i) in pageText" class="mb-4">
+      <div v-for="(ayah, i) in pageText" class="mb-6">
         <div v-if="pageAyat[i].isFirstOfSurah" class="mb-3">
           سوره {{ pageAyat[i].surah.name }}
         </div>
 
         <div class="flex items-center">
-          <div class="mr-3">
-            {{ pageAyat[i].index }}
+          <div class="mr-3 flex flex-col">
+            <span class="flex flex-col">
+              <span class="text-xs">اندیس:</span>
+              <span class="font-bold">{{ pageAyat[i].index }}</span>
+            </span>
+            <span class="text-xs">
+              ({{ pageAyat[i].ayahNumber }}&nbsp;{{ pageAyat[i].surah.name }})
+            </span>
           </div>
           <div>
             <span
               v-for="(word, offset) in ayah"
-              class="border-1 border-gray-500 group border-solid relative mx-1 rounded inline-block px-1 mb-1 py-0.5"
+              class="group border-1 text-lg group border-gray-500 border-solid relative mx-1 rounded inline-block px-1.5 mb-1.5 py-0.5"
             >
               {{ word }}
               <span
-                class="hidden cursor-default text-bold group-hover:flex absolute bg-green-500/80 items-center justify-center inset-0"
+                class="absolute font-bold bg-white border group-hover:scale-200 border-gray-500 border-solid transition-transform text-10px w-4 h-4 leading-4 rounded-full flex items-center justify-center left-0 top-0 transform -translate-y-1/2 translate-x-1/2"
               >
                 {{ offset }}
               </span>
-            </span>
-            <span class="text-xs">
-              {{ pageAyat[i].ayahNumber }}&nbsp;{{ pageAyat[i].surah.name }}
             </span>
           </div>
         </div>
